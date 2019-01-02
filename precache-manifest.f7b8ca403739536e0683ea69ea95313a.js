7f60b25e35a31fe7b3fcb871e53eb50b0246fc76
Ntonio36 <32229257+Ntonio36@users.noreply.github.com>
2019-01-02 03:56:28 +0200

    Updates

---
+++ precache-manifest.f7b8ca403739536e0683ea69ea95313a.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7055661e3d7ce7623904b42978d4799",
+    "url": "/react-objection/static/media/apollo.f7055661.gif"
+  },
+  {
+    "revision": "f9bf035fa952701746b4",
+    "url": "/react-objection/static/css/main.13925203.chunk.css"
+  },
+  {
+    "revision": "3b74827fe8b5d30a57b4",
+    "url": "/react-objection/static/js/1.3b74827f.chunk.js"
+  },
+  {
+    "revision": "b7ca6d2d82e151e18e19",
+    "url": "/react-objection/static/js/runtime~main.b7ca6d2d.js"
+  },
+  {
+    "revision": "3de6d934a4f31d4b7abe760109606d35",
+    "url": "/react-objection/static/media/Objection_HD.3de6d934.png"
+  },
+  {
+    "revision": "9962d1c4ddd1c8b4e169310d44823efe",
+    "url": "/react-objection/static/media/franny.9962d1c4.png"
+  },
+  {
+    "revision": "ef64056333e9c10728e90359dcc82b21",
+    "url": "/react-objection/static/media/karma.ef640563.png"
+  },
+  {
+    "revision": "a21f08da47fa0f86cd3c265940c303e9",
+    "url": "/react-objection/static/media/edgeworth.a21f08da.png"
+  },
+  {
+    "revision": "1a1348aba41e4bd9eb5c28580ff8232a",
+    "url": "/react-objection/static/media/payne1.1a1348ab.png"
+  },
+  {
+    "revision": "2724557dfdeda9d09e32009a42c79198",
+    "url": "/react-objection/static/media/mia.2724557d.png"
+  },
+  {
+    "revision": "b1d153274440b4addc083e0ec15c3e8b",
+    "url": "/react-objection/static/media/defense4.b1d15327.png"
+  },
+  {
+    "revision": "cd84536c10f78dd9f23faa3f2f35c363",
+    "url": "/react-objection/static/media/prosecution4.cd84536c.png"
+  },
+  {
+    "revision": "f95126ffe8d1097d2b1ad5a8d5fbf71c",
+    "url": "/react-objection/static/media/aceattorney.f95126ff.ttf"
+  },
+  {
+    "revision": "36a683d4485094481aeaa3cd58a632cd",
+    "url": "/react-objection/static/media/Apollo Justice.36a683d4.ogg"
+  },
+  {
+    "revision": "0f708a049152c523840f62fdfad2e55c",
+    "url": "/react-objection/static/media/Franziska von Karma.0f708a04.ogg"
+  },
+  {
+    "revision": "be76363cf1667f19452700aa19741b97",
+    "url": "/react-objection/static/media/Godot.be76363c.ogg"
+  },
+  {
+    "revision": "460d48dd8a2d8d530c43a60ce104a08e",
+    "url": "/react-objection/static/media/Klavier Gavin.460d48dd.ogg"
+  },
+  {
+    "revision": "e064209935a32d7523192d292ef3d53f",
+    "url": "/react-objection/static/media/Manfred von Karma.e0642099.ogg"
+  },
+  {
+    "revision": "12f802fdede193270a724d82c157c3b1",
+    "url": "/react-objection/static/media/Mia Fey.12f802fd.ogg"
+  },
+  {
+    "revision": "1c3886ae63ddbe306b4acecf7840749d",
+    "url": "/react-objection/static/media/Miles Edgeworth.1c3886ae.ogg"
+  },
+  {
+    "revision": "5eaf88c9fcee641aebd9fd657bd102aa",
+    "url": "/react-objection/static/media/Phoenix Wright.5eaf88c9.ogg"
+  },
+  {
+    "revision": "e75b80e2afa904951a6dbff9c91ab51a",
+    "url": "/react-objection/static/media/Winston Payne.e75b80e2.ogg"
+  },
+  {
+    "revision": "3396c79e91cf9bf8ada16fdc2aea1c63",
+    "url": "/react-objection/static/media/blipfemale.3396c79e.ogg"
+  },
+  {
+    "revision": "3f98ab58a8d14b8873cc70d510481dfb",
+    "url": "/react-objection/static/media/blipmale.3f98ab58.ogg"
+  },
+  {
+    "revision": "f9bf035fa952701746b4",
+    "url": "/react-objection/static/js/main.f9bf035f.chunk.js"
+  },
+  {
+    "revision": "162b2a60e848eacc42d0bc9e0c894820",
+    "url": "/react-objection/static/media/edgeworth.162b2a60.gif"
+  },
+  {
+    "revision": "a5a7975757acdf07e0b108d3dd778ea8",
+    "url": "/react-objection/static/media/edgeworth2.a5a79757.gif"
+  },
+  {
+    "revision": "956d712d80d73ae91e378df4e67115ba",
+    "url": "/react-objection/static/media/franny.956d712d.gif"
+  },
+  {
+    "revision": "40b64b4556728d996d9ec6771764a9bb",
+    "url": "/react-objection/static/media/karma.40b64b45.gif"
+  },
+  {
+    "revision": "601c93486972c374ed5ef550d9254afc",
+    "url": "/react-objection/static/media/klavier.601c9348.gif"
+  },
+  {
+    "revision": "04aa74659d67e5cda82d4474d1715f74",
+    "url": "/react-objection/static/media/mia.04aa7465.gif"
+  },
+  {
+    "revision": "b8cfcc4c2e90656f6666da93427343ca",
+    "url": "/react-objection/static/media/payne.b8cfcc4c.gif"
+  },
+  {
+    "revision": "7fc753b12e7ce09fd3ba62b43c7b2f8d",
+    "url": "/react-objection/static/media/payne2.7fc753b1.gif"
+  },
+  {
+    "revision": "fb866fd69cafe568f7a4afb2ad1a5886",
+    "url": "/react-objection/static/media/phoenix.fb866fd6.gif"
+  },
+  {
+    "revision": "5125c0d1b47075525933854e7c67e799",
+    "url": "/react-objection/static/media/apollo.5125c0d1.gif"
+  },
+  {
+    "revision": "16cd5660280b0069a57fa8f966a21fef",
+    "url": "/react-objection/static/media/edgeworth.16cd5660.gif"
+  },
+  {
+    "revision": "e400034211a67d84cd03e8dc7a4dc33b",
+    "url": "/react-objection/static/media/edgeworth2.e4000342.gif"
+  },
+  {
+    "revision": "85a4b65a44efe0253ef8c8ca321fde94",
+    "url": "/react-objection/static/media/franny.85a4b65a.gif"
+  },
+  {
+    "revision": "a79dc882ff27788d76ce438c021c0d73",
+    "url": "/react-objection/static/media/godot.a79dc882.gif"
+  },
+  {
+    "revision": "92012d454e4050729ac7bf92c022a7e6",
+    "url": "/react-objection/static/media/karma.92012d45.gif"
+  },
+  {
+    "revision": "7b34c46e94e502c8dbc37f3c5eb0363a",
+    "url": "/react-objection/static/media/klavier.7b34c46e.gif"
+  },
+  {
+    "revision": "f9dfd398e3f61565442a811632ed6ef8",
+    "url": "/react-objection/static/media/mia.f9dfd398.gif"
+  },
+  {
+    "revision": "4515175301629fd02369f4777138bee0",
+    "url": "/react-objection/static/media/payne.45151753.gif"
+  },
+  {
+    "revision": "ec3dc137f0a90ac8c16d0cdba31ba735",
+    "url": "/react-objection/static/media/payne2.ec3dc137.gif"
+  },
+  {
+    "revision": "6594fbe693af7f028fd3cefeadcb634c",
+    "url": "/react-objection/static/media/payne3.6594fbe6.gif"
+  },
+  {
+    "revision": "3e52c959c17ce707f830503345bc911d",
+    "url": "/react-objection/static/media/phoenix.3e52c959.gif"
+  },
+  {
+    "revision": "79a4d4d9cd9422474506d1025cc81566",
+    "url": "/react-objection/static/media/blipfemale.79a4d4d9.ogg"
+  },
+  {
+    "revision": "2078278994398f327eea55793fb27049",
+    "url": "/react-objection/static/media/blipmale.20782789.ogg"
+  },
+  {
+    "revision": "5262efa8760ff0b0304895af8c4e0a05",
+    "url": "/react-objection/index.html"
+  }
+];
